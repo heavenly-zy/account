c3ab95ed1232e5b757c7ed21abd0c05094abab2a
zy <1457715339@qq.com>
2020-03-07 12:53:14 +0800

    修改为 .sync 的形式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,14 +1,10 @@
 <template>
   <Layout class-prefix="layout">
     {{record}}
-    <NumberPad :value="record.amount" @update:value="onUpdateAmount" />
-    <Types :value="record.type" @update:value="onUpdateType" />
+    <NumberPad :value.sync="record.amount" />
+    <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
-    <Tags
-      :data-source="tags"
-      @update:dataSource="onUpdateTotalTags"
-      @update:value="onUpdateSelectedTags"
-    />
+    <Tags :data-source.sync="tags" @update:value="onUpdateSelectedTags" />
   </Layout>
 </template>
 
@@ -35,17 +31,8 @@ export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
   record: Record = { tags: [], notes: "", type: "-", amount: 0 }; // 初始值
 
-  onUpdateAmount(value: number) {
-    this.record.amount = value;
-  }
-  onUpdateType(value: string) {
-    this.record.type = value;
-  }
   onUpdateNotes(value: string) {
     this.record.notes = value;
-  }
-  onUpdateTotalTags(value: string[]) {
-    this.tags = value;
   }
   onUpdateSelectedTags(value: string[]) {
     this.record.tags = value;
